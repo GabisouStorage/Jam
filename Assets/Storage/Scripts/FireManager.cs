88f12b3c7f387d72898ca50880e5bfc63bbe7b66
yagoazedias <yagoluis9@hotmail.com>
2015-10-30 02:24:39 -0200

    Version 0.0

---
+++ Assets/Storage/Scripts/FireManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireManager : MonoBehaviour
+{
+
+    // Variebles to manager the fired.
+    public Transform actualPos;
+    private Transform destiny;
+    public float distance;
+    public float angle;
+    public float speed;
+    private bool isFired;
+    private bool isAreadyshoot;
+    private Vector3 destinePos;
+    private Vector2 destinyVect2;
+    private int count;
+    private Vector2 _direction;
+
+    void Start()
+    {
+        speed = 50f;
+    }
+
+    void rotate()
+    {
+        //olhar para o player
+        //usando "PlayerVect2" podemos iguala-lo ao vector3 player
+        //assim podemos usar o player(vector3), como um vector2 dando fim aos bugs que ocorreriam por ser um sprite
+        //o resto é matemática
+        destinyVect2 = new Vector2(destinePos.x, destinePos.y);
+        _direction = (destinyVect2 - (Vector2)transform.position).normalized;
+        angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg + (int)0;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    void Fired()
+    {
+         isFired = true; 
+
+        if (isFired)
+        { ShotTheFire(true); count++; }
+
+    }
+
+    void ShotTheFire(bool follow)
+    {
+        if (follow)
+        {
+            Vector3 direction = new Vector3 ((Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized.x, (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized.y, this.transform.position.z);
+            transform.position += direction * Time.deltaTime * speed;
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Fired();
+        rotate();
+    }
+}
---
+++ Assets/Storage/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    public GameObject fire;
+
+    void SpawFire()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Instantiate(fire, this.transform.position, Quaternion.identity);
+        }
+    }
+
+	void FixedUpdate ()
+    {
+        SpawFire();
+    }
+}
